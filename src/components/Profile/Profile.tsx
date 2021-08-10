305cfdb10fae5ce5dcca0384d309749f75abd1ac
frostwOw13 <frostwow13@yandex.ru>
2021-08-10 20:45:28 +0300

    feat: add rendering contacts list

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,46 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Profile.scss';
 
-const Profile: React.FC = () => (
-  <div className="vh-100">
-    <div className="container py-5">
-      <div className="row d-flex justify-content-center align-items-center h-100">
-        <div className="col col-xl-10">
-          <div className="card bg-dark">
-            <div className="card-body p-5">
-              <form className="d-flex justify-content-center align-items-center mb-4">
-                <div className="form-outline flex-fill">
-                  <label className="form-label col-12" htmlFor="form1">
-                    <input
-                      className="form-control"
-                      id="form1"
-                      placeholder="New task..."
-                      type="text"
-                    />
-                  </label>
-                </div>
-                <button className="btn btn-info ms-2" type="button">Add</button>
-              </form>
+interface ContactData {
+  name: string,
+  phone: string
+}
 
-              <div className="tab-content" id="ex1-content">
-                <div
-                  aria-labelledby="ex1-tab-1"
-                  className="tab-pane fade show active"
-                  id="ex1-tabs-1"
-                >
-                  <ul className="list-group mb-0">
-                    <li className="list-group-item d-flex align-items-center border-0 mb-2 rounded">
+const Profile: React.FC = () => {
+  const [contactsList, setContactsList] = useState<Array<ContactData>>([{name: "John", phone: "8-999-999-99-99"}]);
+  const [contact, setContact] = useState<ContactData>({name: "", phone: ""});
+
+  const submitHandler = () => {
+    setContactsList([...contactsList, contact]);
+  };
+
+  return (
+    <div className="vh-100">
+      <div className="container py-5">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col col-xl-10">
+            <div className="card bg-dark">
+              <div className="card-body p-5">
+                <form className="d-flex justify-content-center align-items-center mb-4">
+                  <div className="form-outline flex-fill">
+                    <label className="form-label col-6" htmlFor="form1">
                       <input
-                        aria-label="..."
-                        className="form-check-input me-2"
-                        type="checkbox"
-                        value=""
+                        className="form-control col-6"
+                        id="form1"
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                          setContact({...contact, name: e.target.value});
+                        }}
+                        placeholder="Name"
+                        type="text"
+                        value={contact.name}
                       />
-                      <s>Cras justo odio</s>
-                      <div className="btn-item" id="edit" />
-                      <div className="btn-item" id="delete"/>
-                    </li>
-                  </ul>
+                      <input
+                        className="form-control col-6"
+                        id="form2"
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                          setContact({...contact, phone: e.target.value});
+                        }}
+                        placeholder="Phone number"
+                        type="text"
+                        value={contact.phone}
+                      />
+                    </label>
+                  </div>
+                  <button
+                    className="btn btn-info ms-2"
+                    onClick={submitHandler}
+                    type="button">Add</button>
+                </form>
+
+                <div className="tab-content" id="ex1-content">
+                  <div
+                    aria-labelledby="ex1-tab-1"
+                    className="tab-pane fade show active"
+                    id="ex1-tabs-1"
+                  >
+                    <ul className="list-group mb-0">
+                      {contactsList.map((val) => (
+                        <li className="list-group-item d-flex align-items-center border-0 mb-2 rounded">
+                          <div className="info">
+                            <p>{val.name}</p>
+                            <p>{val.phone}</p>
+                          </div>
+                          <div className="btn-item" id="edit" />
+                          <div className="btn-item" id="delete"/>
+                        </li>
+                        )
+                      )}
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
@@ -48,7 +79,7 @@ const Profile: React.FC = () => (
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Profile;
